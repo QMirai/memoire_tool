42e830e0d9c5ce161e7eb88e75d080663929f861
ChristopheQ33 <127989939+QMirai@users.noreply.github.com>
2023-11-27 21:37:38 -0500

    use for loop to fre_control, fre_raising

--- batch_grew.sh
+++ batch_grew.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-input_dir="/mnt/hgfs/share/list_fre_raising_verb"
-output_dir="/mnt/hgfs/share/output_list_fre_raising_verb"
-
 grew compile -i eng_fre_sud.json
-mkdir -p "$output_dir"
 
-for file in "$input_dir"/*.req
+for type in "raising_verb" "control_verb"
 do
-	echo "Processing $file"
-	filename=$(basename -- "$file")
-	filename="${filename%.*}"
-	grew grep -request $file -i eng_fre_sud.json > "$output_dir"/output_sud_"$filename".json
+	input_dir="/mnt/hgfs/share/list_fre_${type}"
+	echo $input_dir
+	output_dir="/mnt/hgfs/share/output_${input_dir##*/}"
+	mkdir -p "$output_dir"
+
+	for file in "$input_dir"/*.req
+	do
+		echo "Processing $file"
+		filename=$(basename -- "$file")
+		filename="${filename%.*}"
+		grew grep -request $file -i eng_fre_sud.json > "$output_dir"/output_sud_"$filename".json
+	done
 done
 
 echo "Finished!"
